90809ec86084f2fa902dbce4ae679dad25dc2e00
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2022-06-11 15:18:21 -0300

    Update Bitmap.java

--- Bitmap.java
+++ Bitmap.java
@@ -12,7 +12,7 @@ public class desenho {
     		
 		
 	for( i = 0;i < 16 ; i++) { // dois for para percorrer toda matriz para fazer a execucao para imprimir a mensagem ao usuario
-    System.out.printf("informe os elemntos %da linha/n", (i+1));
+    System.out.printf("informe os elementos %da linha/n", (i+1));
 		for( j = 0; j<16 ; j ++) {
 			System.out.printf("m[%d][%d] = " , i, j);
 			m[i][j] =ler.nextInt();
